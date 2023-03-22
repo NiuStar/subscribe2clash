9d085e1abb90a59ef7bb0611434594cc1a303b59
Nqc <24802117@qq.com>
2023-03-22 16:43:23 +0800

    修改优化3

--- model/connect.go
+++ model/connect.go
@@ -5,13 +5,18 @@ import (
 	"gorm.io/driver/mysql"
 	_ "gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 var db *gorm.DB
 
 func init() {
+	mysqlString := os.Getenv("mysql")
+	if len(mysqlString) == 0 {
+		mysqlString = "127.0.0.1"
+	}
 	var err error
-	dialector := mysql.Open("root:12345678@tcp(127.0.0.1:3306)/subscribe2clash?charset=utf8mb4&parseTime=True&loc=Local")
+	dialector := mysql.Open(fmt.Sprintf("root:12345678@tcp(%s:3306)/subscribe2clash?charset=utf8mb4&parseTime=True&loc=Local", mysqlString))
 	db, err = gorm.Open(dialector)
 	if err != nil {
 		fmt.Println("连接数据库失败，请检查参数：", err)
